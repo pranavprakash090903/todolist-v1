13ac6987579e245debb47c9b703898bc75b77749
unknown <pranavprakash090903@gmail.com>
2023-07-25 17:14:13 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyparser=require("body-parser");
+
+const app=express();
+var items=["Buy Food","Cook Food","Eat Food"];
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/",function(req,res){
+
+  let today= new Date();
+
+  let options= {
+    weekday:"long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day= today.toLocaleDateString("en-US",options);
+  res.render("list",{KindOfDay:day , newListItems:items});
+});
+app.post("/",function(req, res){
+ let item = req.body.newItem;
+ items.push(item);
+ res.redirect("/");
+});
+
+
+ app.listen(process.env.PORT || 3000,function(){
+   console.log("Server started on port 3000");
+ })
